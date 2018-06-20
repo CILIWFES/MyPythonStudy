f048e1bd2de9bdfd2572df25095af8f6afee227e
NameNotNull <NameNotNull@163.com>
2018-06-20 14:04:47 +0800

    闭包

--- start/Start.py
+++ start/Start.py
@@ -19,8 +19,9 @@ startTime=datetime.datetime.now()#起始时间
 
 #import  dataStruct.tuple.tuple
 # import  dataStruct.string.String
-import grammar.grammar
+# import grammar.grammar
 # import classTest.classTest
+import Thread.thread
 ###################################################################################################################
 endTime=datetime.datetime.now()#终止时间
 print("""\n-------------------------------------------------------------------------------------
