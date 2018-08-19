e2378fc957edf95ebfe971a5db2e95a6acb2f78d
CILILWFES <CILIWFES@163.com>
2018-08-19 22:44:41 +0800

    text

---
+++ TextClassification/makeBunch.py
@@ -0,0 +1,109 @@
+from lxml import etree, html
+import os
+import sys
+
+print(sys.getdefaultencoding())
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0] + "/Support/chapter02"
+
+# 初识别lxml
+# path = rootPath + '/1.html'
+# content = open(path, 'rb').read()
+# page = html.document_fromstring(content)  # 解析文件
+# text = page.text_content()  # 去除标签
+import jieba
+
+'''
+# 初识别jieba
+
+# 默认切分
+segList = jieba.cut("张三1996年毕业于北京五道口职业大学", cut_all=False)
+print("Default Mode:", " ".join(segList))  # segList为序列," "与序列每个元素拼接
+# 默认的和上面一样
+segList = jieba.cut("张三1996年毕业于北京五道口职业大学")
+print(" ".join(segList))
+# 全切分
+segList = jieba.cut("张三1996年毕业于北京五道口职业大学", cut_all=True)
+print("Full Mode:", " ".join(segList))
+# 搜索引擎模式切分
+segList = jieba.cut_for_search("小明毕业于中国科学院计算机所,后在日本京都大学深造")
+print(" ".join(segList))
+
+'''
+# print(text)
+
+
+# 保存文件
+def saveFile(savePath, content):
+    content = content.encode(encoding='utf-8')#解码为字节码
+    fp = open(savePath, "wb")
+    fp.write(content)
+    fp.close()
+
+
+# 读取文件
+def readFile(readPath):
+    fp = open(readPath, "rb")
+    content = fp.read()
+    content = content.decode(encoding='utf-8')#解码为字符码
+    fp.close()
+    return content
+
+#转化语料库遍历提取语料
+corpusPath = rootPath + "/train_corpus_small"  # 未分词语料路径
+segPath = rootPath + "/train_corpus_seg"  # 已分词的语料路径
+
+# 获取语料库下的文件路径
+cateList = os.listdir(corpusPath)
+
+for fileDir in cateList:
+    classPath = corpusPath + "/" + fileDir + "/"
+    testPath = segPath + "/" + fileDir + "/"
+    if not os.path.exists(testPath):
+        os.makedirs(testPath)  # 若不存在则创建目录
+    fileList = os.listdir(classPath)  # 目录下所有文件
+    for fileName in fileList:
+        fullName = classPath + fileName  # 文件夹全名
+        content = readFile(fullName).strip()
+        content = content.replace("\r\n", "").strip()  # 删除换行与多余的空格
+        content_seg = jieba.cut(content)
+        saveFile(testPath + fileName, " ".join(content_seg))
+
+
+import pickle
+
+from  sklearn.datasets.base import Bunch
+wordbagPath=rootPath+"/train_word_bag/train_set.dat"
+
+
+bunch=Bunch(target_name=[],label=[],fileNames=[],contents=[])
+segPath=rootPath+"/train_corpus_seg/"
+cateList=os.listdir(segPath)#将分类信息保存至Bunch中
+bunch.target_name.extend(cateList)#类别信息
+
+for mydir in cateList:
+    classPath=segPath+mydir
+    fileList=os.listdir(classPath)
+    for fileName in fileList:
+        fullName=classPath+"/"+fileName
+        bunch.label.append(mydir)#保存当前文件的分类标签
+        bunch.fileNames.append(fullName)#当前文件路径
+        bunch.contents.append(readFile(fullName).strip())
+
+#Bunch对象持久化
+fileObj=open(wordbagPath,'wb')
+pickle.dump(bunch,fileObj)
+print(bunch.label)
+fileObj.close()
+
+print("构建Bunch对象完成")
+#对象读取持久化序列
+fileObj=open(wordbagPath,'rb')
+bunch=pickle.load(fileObj)
+print(bunch.label)
+
+
+stopworgPath=rootPath+"/train_word_bag/hlt_stop_words.txt"
+stopList=readFile(stopworgPath).splitlines()
+print(stopList)
---
+++ TextClassification/makeTfidfSpace.py
@@ -0,0 +1,72 @@
+import sys
+import os
+from sklearn.datasets.base import Bunch
+import pickle
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0] + "/Support/chapter02"
+
+
+# 读取文件
+def readFile(readPath):
+    fp = open(readPath, "rb")
+    content = fp.read()
+    content = content.decode(encoding='utf-8')  # 解码为字符码
+    fp.close()
+    return content
+
+
+stopworgPath = rootPath + "/train_word_bag/hlt_stop_words.txt"
+stopList = readFile(stopworgPath).splitlines()
+
+print(sys.getdefaultencoding())
+
+
+def readBunch(path):
+    fileObj = open(path, "rb")
+    bunch = pickle.load(fileObj)
+    fileObj.close()
+    return bunch
+
+
+def writeBunch(path, bunch):
+    fileObj = open(path, "wb")
+    pickle.dump(bunch, fileObj)
+    fileObj.close()
+
+
+path = rootPath + "/train_word_bag/train_set.dat"
+bunch = readBunch(path)
+print(bunch.contents[0])
+
+tfidfSpace = Bunch(targetName=bunch.target_name, label=bunch.label, fileNames=bunch.fileNames, contents=bunch.contents,
+                   tdm=[], vocabulary=[])
+'''
+max_df：这个给定特征可以应用在 tf-idf 矩阵中，用以描述单词在文档中的最高出现率。假设一个词（term）在 80% 的文档中都出现过了，那它也许（在剧情简介的语境里）只携带非常少信息。
+min_df：可以是一个整数（例如5）。意味着单词必须在 5 个以上的文档中出现才会被纳入考虑。设置为 0.2；即单词至少在 20% 的文档中出现 。
+sublinear_tf:应用子线性tf缩放，即用1 + log（tf）替换tf。
+'''
+vectorizer = TfidfVectorizer(stop_words=stopList, sublinear_tf=True, max_df=0.5)
+
+
+'''
+第一种方法是在用CountVectorizer类向量化之后再调用TfidfTransformer类进行预处理。
+第二种方法是直接用TfidfVectorizer完成向量化与TF-IDF预处理。
+这里是用第二种
+'''
+# transformer = TfidfTransformer()
+
+#词频矩阵(TF-IDF)
+tfidfSpace.tdm = vectorizer.fit_transform(bunch.contents)
+#Tf-idf-weighted  tf-idf的逆文件权重:  (第i-1个文件,vocabulary索引)  权重   的结构
+
+# print(tfidfSpace.tdm.todense())#所有的词向量如0.74, 0.11, 0,0 0,0 0 0,0.54,0.55,0.001,0.052
+
+tfidfSpace.vocabulary = vectorizer.vocabulary_#所有的词向量索引,如"xx":1表示上面对应的0.11项的词是"xx"
+
+new_dict = {v:k for k,v in tfidfSpace.vocabulary.items()}#转置字典索引
+#new_dict[57514] '石像'
+spacePath = rootPath + "/train_word_bag/tfdifspace.dat"
+writeBunch(spacePath, tfidfSpace)
