9a89e64c77a057a73e9cf68ee760f8b4dfeafa76
CILILWFES <CILIWFES@163.com>
2018-08-20 22:53:12 +0800

    text

---
+++ TextClassification/Nbayes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import sys  
+import os 
+import time
+from numpy import *
+import numpy as np
+from TextClassification.Nbayes_lib import *
+
+
+# 配置utf-8输出环境
+sys.getdefaultencoding()
+
+dataSet,listClasses = loadDataSet()
+nb = NBayes()
+nb.train_set(dataSet,listClasses)
+nb.map2vocab(dataSet[3])
+print(nb.predict(nb.testset))
