f0233427c38b8f68c06471c6e74942d687719fd2
CILILWFES <CILIWFES@163.com>
2018-09-02 22:48:38 +0800

    CART

--- DecisionTree/CART.py
+++ DecisionTree/CART.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from math import pow
-import numpy as np
+
 
 class CART(object):
     def __init__(self):
@@ -11,8 +11,6 @@ class CART(object):
 
     # 主函数入口
     def train(self, trainSet, classList, isDiscrete):
-        trainSet=np.mat(trainSet)
-        classList=np.mat(classList)
         # 选择要处理的数据类型,并对离散型数据进行转化
         self.selectFunction(isDiscrete)
         self.classList = classList[:]
@@ -25,15 +23,15 @@ class CART(object):
             self.function = self.Continuous
 
     # 离散型 计算Gini基尼指数
-    def Discrete(self, vec1, vec2):
-        vec1=vec1.T
-        vec2=vec2.T
+    def Discrete(self, lst1, vec2):
+        lst1 = lst1.T
+        vec2 = vec2.T
         # calculate freqency
-        fre1 = list(Counter(vec1.tolist()[0]).values())
+        fre1 = list(Counter(lst1.tolist()[0]).values())
         fre2 = list(Counter(vec2.tolist()[0]).values())
 
-        size1 = float(np.alen(vec1))
-        size2 = float(np.alen(vec2))
+        size1 = float(len(lst1))
+        size2 = float(len(vec2))
         result1 = 1
         result2 = 1
 
@@ -48,26 +46,30 @@ class CART(object):
         return result2 + result1
 
     # 连续型,计算方差
-    def Continuous(self, vec1, vec2):
-        vec1=vec1.astype(float).T
-        vec2=vec2.astype(float).T
-        avg1 = np.sum(vec1) / np.alen(vec1)
-        avg2 = np.sum(vec2) / np.alen(vec2)
+    def Continuous(self, lst1, lst2):
+        avg1 = sum(lst1) / len(lst1)
+        avg2 = sum(lst2) / len(lst2)
         result1 = 0
         result2 = 0
-        for item in vec1.tolist()[0]:
+        for item in lst1.tolist()[0]:
             result1 += pow(item - avg1, 2)
-        result1 = pow(result1 / float(np.alen(vec1)), 0.5)
+        result1 = pow(result1 / float(len(lst1)), 0.5)
 
-        for item in vec2.tolist()[0]:
+        for item in lst2.tolist()[0]:
             result2 += pow(item - avg2, 2)
-        result2 = pow(result2 / float(np.alen(vec2)), 0.5)
+        result2 = pow(result2 / float(len(lst2)), 0.5)
         return result1 + result2
 
     def buildTree(self, trainSet, classList):
+        classSet = [data[-1] for data in trainSet]
+        for indx in range(len(classSet)):
+            # 先按列切割
+            allLst = [data[indx] for data in trainSet]
+            allSet = set(allLst)
+            lst = []
+            for item in allSet:
+                lst.append([data for data in allLst if data == item])
 
-        for indx in range(np.shape(trainSet)[1]):
-            lst=trainSet[:,indx].T.tolist()[0]
-            for item in set(lst):
-
-
+            # 开始选择
+            for item in lst:
+                otherLst
