6e1214b4a9287feacc78ccb59e5d593994868ff9
NameNotNull <NameNotNull@163.com>
2018-09-03 14:01:45 +0800

    CART

--- DecisionTree/CART.py
+++ DecisionTree/CART.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from math import pow
-import numpy as np
+
 
 class CART(object):
     def __init__(self):
@@ -11,8 +11,6 @@ class CART(object):
 
     # 主函数入口
     def train(self, trainSet, classList, isDiscrete):
-        trainSet=np.mat(trainSet)
-        classList=np.mat(classList)
         # 选择要处理的数据类型,并对离散型数据进行转化
         self.selectFunction(isDiscrete)
         self.classList = classList[:]
@@ -25,15 +23,14 @@ class CART(object):
             self.function = self.Continuous
 
     # 离散型 计算Gini基尼指数
-    def Discrete(self, vec1, vec2):
-        vec1=vec1.T
-        vec2=vec2.T
+    # lst1与ls2皆为预测项
+    def Discrete(self, lst1, lst2):
         # calculate freqency
-        fre1 = list(Counter(vec1.tolist()[0]).values())
-        fre2 = list(Counter(vec2.tolist()[0]).values())
+        fre1 = list(Counter(lst1).values())
+        fre2 = list(Counter(lst2).values())
 
-        size1 = float(np.alen(vec1))
-        size2 = float(np.alen(vec2))
+        size1 = float(len(lst1))
+        size2 = float(len(lst2))
         result1 = 1
         result2 = 1
 
@@ -48,26 +45,65 @@ class CART(object):
         return result2 + result1
 
     # 连续型,计算方差
-    def Continuous(self, vec1, vec2):
-        vec1=vec1.astype(float).T
-        vec2=vec2.astype(float).T
-        avg1 = np.sum(vec1) / np.alen(vec1)
-        avg2 = np.sum(vec2) / np.alen(vec2)
+    #lst1与ls2皆为预测项
+    def Continuous(self, lst1, lst2):
+        avg1 = sum(lst1) / len(lst1)
+        avg2 = sum(lst2) / len(lst2)
         result1 = 0
         result2 = 0
-        for item in vec1.tolist()[0]:
+        for item in lst1:
             result1 += pow(item - avg1, 2)
-        result1 = pow(result1 / float(np.alen(vec1)), 0.5)
+        result1 = pow(result1 / float(len(lst1)), 0.5)
 
-        for item in vec2.tolist()[0]:
+        for item in lst2:
             result2 += pow(item - avg2, 2)
-        result2 = pow(result2 / float(np.alen(vec2)), 0.5)
+        result2 = pow(result2 / float(len(lst2)), 0.5)
         return result1 + result2
 
     def buildTree(self, trainSet, classList):
 
-        for indx in range(np.shape(trainSet)[1]):
-            lst=trainSet[:,indx].T.tolist()[0]
-            for item in set(lst):
+        classSet = [data[-1] for data in trainSet]
+        #递归结束判断
+        #1.
+        #2.
+
+        minSelect=(0,"")
+        #初始越大越好
+        minValue=100
+        #遍历条件,选择最优切分条件与最优切分点
+        for indx in range(len(trainSet[0])):
+            lst = [data[indx] for data in trainSet[indx]]
+            featureSet = set(lst)
+
+            del lst
+            featureDict = {}
+            # 获取分割的分类
+            for feature in featureSet:
+                tempLst = [data[-1] for data in trainSet if data[indx] == feature]
+                featureDict[feature] = tempLst
+
+            del featureSet
+            #选择最小的分类
+            minFeatureValue = 1.1
+            minFeature = ''
+            for feature, lst in featureDict.items():
+                otherList = [data[-1] for data in trainSet if data[indx] != feature]
+                returnValue = self.function(lst)
+                if returnValue < minFeatureValue:
+                    minFeatureValue = returnValue
+                    minFeature=feature
+            if minFeatureValue<minValue:
+                minValue=minFeatureValue
+                minSelect=(indx,minFeature)
+        #下面要针对切分点进行切分
+
+        #切分时最好能把最优切分特征列给删除
+
+        #建立tree,放入最优节点
+
+        #递归左节点
+        #递归右节点
+        #return
+
 
 
